5cc59e338f2d81fe969d8c9643fbab402f77366a
nishaj0 <njnishaj0@gmail.com>
2024-03-10 18:10:12 +0530

    feat: add camera and other basic set up, handled window resize

---
+++ src/script.js
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+
+/**
+ * Base
+ */
+
+// canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// scene
+const scene = new THREE.Scene();
+
+/**
+ * Camera
+ */
+const sizes = {
+     width: window.innerWidth,
+     height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(
+     75,
+     sizes.width / sizes.height,
+     0.1,
+     100
+);
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 5;
+scene.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+     canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// handle window resize
+window.addEventListener("resize", () => {
+   // update sizes
+   sizes.width = window.innerWidth;
+   sizes.height = window.innerHeight;
+
+   // update camera
+   camera.aspect = sizes.width / sizes.height;
+   camera.updateProjectionMatrix();
+
+   // update renderer
+   renderer.setSize(sizes.width, sizes.height);
+});
