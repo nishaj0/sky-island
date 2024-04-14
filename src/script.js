9a5f9ec692ebf1c00061a68c57aa0c0f7a1fd1b6
nishaj0 <njnishaj0@gmail.com>
2024-04-14 19:43:14 +0530

    feat(objects): create home with geometries
    add lil-gui

--- src/script.js
+++ src/script.js
@@ -1,9 +1,15 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { threshold } from "three/examples/jsm/nodes/Nodes.js";
+import GUI from "lil-gui";
+
 /**
  * Base
  */
+
+/**
+ * debug ui
+ */
+const gui = new GUI();
 
 // canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -11,12 +17,75 @@ const canvas = document.querySelector("canvas.webgl");
 // scene
 const scene = new THREE.Scene();
 
-const testBox = new THREE.Mesh(
-     new THREE.BoxGeometry(4, 3, 4),
-     new THREE.MeshBasicMaterial({ color: "skyblue" })
+/**
+ * Geometry
+ */
+
+// floor
+const floor = new THREE.Mesh(
+     new THREE.CircleGeometry(5, 20),
+     new THREE.MeshBasicMaterial({
+          color: "red",
+          wireframe: true,
+     })
 );
 
-scene.add(testBox);
+floor.rotation.x = -Math.PI * 0.5;
+floor.position.y = 0;
+scene.add(floor);
+
+/**
+ * House
+ */
+const house = new THREE.Group();
+
+house.position.x = -2;
+house.position.z = 2;
+
+scene.add(house);
+
+// walls
+const walls = new THREE.Mesh(
+     new THREE.CylinderGeometry(1.5, 1.5, 1.5, 20),
+     new THREE.MeshBasicMaterial({
+          color: "blue",
+     })
+);
+walls.position.x = 0;
+walls.position.y = 0.75;
+walls.position.z = 0;
+
+house.add(walls);
+
+// roof
+const roof = new THREE.Mesh(
+     new THREE.ConeGeometry(2, 2, 20),
+     new THREE.MeshBasicMaterial({ color: "green" })
+);
+
+roof.position.x = 0;
+roof.position.y = 1 + 1.5; // half of roof height + walls height
+roof.position.z = 0;
+
+house.add(roof);
+
+// door
+const door = new THREE.Mesh(
+     new THREE.PlaneGeometry(0.75, 1),
+     new THREE.MeshBasicMaterial({ color: "purple" })
+);
+
+door.position.x = 1.15;
+door.position.y = 0.6;
+door.position.z = -0.95;
+
+gui.add(door.position, "x").min(-5).max(5).step(0.0001).name("door x");
+gui.add(door.position, "y").min(-5).max(5).step(0.0001).name("door y");
+gui.add(door.position, "z").min(-5).max(5).step(0.0001).name("door z");
+
+door.rotation.y = Math.PI * 0.75;
+
+house.add(door);
 
 /**
  * Camera
@@ -32,9 +101,10 @@ const camera = new THREE.PerspectiveCamera(
      0.1,
      100
 );
-camera.position.x = 4;
-camera.position.y = 2;
+camera.position.x = 3;
+camera.position.y = 3;
 camera.position.z = 5;
+// camera.lookAt(door)
 scene.add(camera);
 
 // controls
@@ -68,7 +138,7 @@ window.addEventListener("resize", () => {
 const clock = new THREE.Clock();
 const tick = () => {
      const elapsedTime = clock.getElapsedTime();
-     
+
      // update controls
      controls.update();
 
